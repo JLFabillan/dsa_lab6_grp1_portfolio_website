5176171372087f7e65514b71c4aa856f459c038d
nug3tsss <ckn.mcnuggets@gmail.com>
2025-11-23 13:25:01 +0800

    Added Search method

--- modules/binary_tree.py
+++ modules/binary_tree.py
@@ -38,8 +38,30 @@ class BinaryTree:
             traversal += (str(start.value) + " ")
         return traversal
 
-    def search(self):
-        pass
+    def search(self, value):
+        # Will return the first node found
+        return self.postorder_search(self.root, value)
+
+    def postorder_search(self, node, value):
+        if node is None:
+            # Tree is empty
+            return None
+        
+        # Search left
+        left_node = self.postorder_search(node.left, value)
+        if left_node:
+            return left_node
+        
+        # Search right
+        right_node = self.postorder_search(node.right, value)
+        if right_node:
+            return right_node
+        
+        # Search node
+        if node.value == value:
+            return node
+        
+        return None
 
     def delete_node(self):
         pass
